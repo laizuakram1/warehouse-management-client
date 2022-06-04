802f3cca419d53d20dc66122d56ed8debe97d6f9
laizuakram1 <laizuakram98@gmail.com>
2022-06-05 00:43:04 +0600

    update delete button

--- src/pages/DisplayAllInventory/DisplayAllInventory.js
+++ src/pages/DisplayAllInventory/DisplayAllInventory.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import './DisplayAllInventory.css';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 const DisplayAllInventory = ({ item }) => {
     const {image, name, price, quantity, supplier, description,} = item;
+
+    const handleDelete = ()=>{
+        console.log('item deleted');
+        window.confirm('are sure delete this product?');
+
+    }
 
    
     return (
@@ -15,11 +23,11 @@ const DisplayAllInventory = ({ item }) => {
                 <p className='fw-bold'>Price:${price}</p>
                 <p>Quantity:{quantity}</p>
                 <p>Supplier:{supplier}</p>
-                <p>description:{description.slice(0, 60)}</p>
+                <p>description:{description}</p>
 
             </div>
-            <div className='mt-5'>
-               <Link to='/delete/:id' >Delete</Link>
+            <div className='mt-5 text-center'>
+            <Button className='delete-Btn' onClick={handleDelete}>Delete</Button>
             </div>
         </div>
     );
